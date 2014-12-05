00ac954c7ac1b23b161fb13cde5546794ee782ab
nh0556 <nelson.huang@onecloudinc.com>
2014-12-04 22:24:30 -0500

    added placeholder file for Stack related commands

---
+++ scale-tester/cmds/stack.py
@@ -0,0 +1,20 @@
+# placeholder for heatclient related with commands
+
+import cmd
+
+class CreateStackCmd(cmd.Command):
+    """
+    This cmd creates a OpenStack Heat Stack instance
+    """
+    
+    def __init__(self):
+        pass
+
+    def init(self):
+        pass
+
+    def execute(self):
+        pass
+
+    def undo(self):
+        pass
