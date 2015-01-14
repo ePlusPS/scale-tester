6f340828c7ce9196d3a318e6dbda21a3f3d935c3
jw <jon.wei@onecloudinc.com>
2015-01-13 17:44:20 -0800

    Set program resources in parse_program

--- scale-tester/cmds/program.py
+++ scale-tester/cmds/program.py
@@ -96,6 +96,8 @@ def parse_program(test_configuration):
 
     program = Program()
     program.context = program_context
+    resources = Resources()
+    program.context['program.resources'] = resources
 
     for command_dict in commands:
         cmd_name = command_dict['command_name']
