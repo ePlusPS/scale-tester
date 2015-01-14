d5cf97fa9af94b25e0192fe3f12691f1c9a848e4
nh0556 <nelson.huang@onecloudinc.com>
2015-01-14 18:36:14 -0500

    corrected typo

--- scale-tester/script/test_heat_update.py
+++ scale-tester/script/test_heat_update.py
@@ -160,7 +160,7 @@ def main():
                            'public_net': 'EX',
                            'public_net_id': '1e04dc1e-958b-4d11-b55f-51593c4606e3'
                           }
-    params['existing'] = True
+    params['existing'] = False
     params['stack_id']= command_line_args.stack_id
     params['template'] = template 
 
