fb5660787400bd1c75f0ed70ce181d84c8825b03
nh0556 <nelson.huang@onecloudinc.com>
2015-02-11 20:45:23 -0500

    updated subinterface port identifier

--- scale-tester/script/subintf_cleanup_gen.py
+++ scale-tester/script/subintf_cleanup_gen.py
@@ -1,7 +1,7 @@
 
 def subintf_cleanup_gen_main():
-    for subintf_num in range(115,3000):
-        print("no interface po10.%s" % subintf_num)
+    for subintf_num in range(104,3000):
+        print("no interface po20.%s" % subintf_num)
 
 if __name__=='__main__':
     subintf_cleanup_gen_main()
